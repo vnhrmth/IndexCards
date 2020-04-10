45b184d21262fd4decfd7fa77a1ab259a5a4290f
VinayJH <vnhrmth@gmail.com>
2020-04-10 16:22:20 +0530

    Backend : Feat : DbContext : Integrating Entity framework and creating and DbContext and other DbModel files.

---
+++ Backend/CardsAPI/Models/DbUser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CardsAPI.Models
+{
+    public class DbUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string MailId { get; set; }
+        public List<DbTopic> Topics { get; set; }
+    }
+}
