5ec39024f70d4fa1ca0b9df37673e29d1bbf16b9
Vinay <vnhrmth@gmail.com>
2020-04-26 20:10:02 +0530

    Backend : Arch : Unit testing : User model class unit testing

---
+++ Backend/CardsAPI/CardsAPITests/ModelsTest/UserTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CardsAPI.Models;
+using NUnit.Framework;
+
+namespace CardsAPITests.ModelsTest
+{
+    public class UserTest
+    {
+        [Test]
+        public void When_email_configuration_object_is_created_then_validate_if_all_properties_are_set()
+        {
+            string id = "1";
+            string mailId = "user@example.com";
+            string name = "User";
+            string password = "password";
+
+            User user = new User
+            {
+                Id = id,
+                MailId = mailId,
+                Name = name,
+                Password = password
+            };
+
+            Assert.AreSame(user.Id, id);
+            Assert.AreSame(user.MailId, mailId);
+            Assert.AreEqual(user.Name, name);
+            Assert.AreSame(user.Password, password);
+        }
+    }
+}
