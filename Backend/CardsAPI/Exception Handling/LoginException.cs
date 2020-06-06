b9c46adfd70caf420f4fd794d94eca3b5f7e6469
Vinay <vnhrmth@gmail.com>
2020-06-06 17:36:52 +0530

    Backend : Arch : Unit Test : Correction of implementation of Signup/Login and internal server error.

---
+++ Backend/CardsAPI/Exception Handling/LoginException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CardsAPI.ExceptionHandling
+{
+    public class LoginException : ApplicationException
+    {
+        public LoginException()
+        {
+        }
+
+        public LoginException(string message) : base(message)
+        {
+        }
+
+        public LoginException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public LoginException(Exception innerException) : base(string.Empty, innerException)
+        {
+        }
+    }
+}
